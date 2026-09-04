7018cb4ef6cb1be1a3a16b265019f650a275d9ce
goncho09 <gonzalolarrica@gmail.com>
2026-09-03 21:11:13 -0300

    chore: migrate to pnpm and add Docker setup

---
+++ dist/config/database.js
@@ -0,0 +1,10 @@
+"use strict";
+// import 'dotenv/config';
+// import { Pool } from 'pg';
+Object.defineProperty(exports, "__esModule", { value: true });
+// export const pool = new Pool({
+//   connectionString: process.env.DB_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
